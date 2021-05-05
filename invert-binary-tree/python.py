c3284e9cae4c32165ce6bee25e6ade4ba5a5ad11
Sean Barry <se@nbarry.co.uk>
2021-05-05 21:20:19 +0100

    invert-binary-tree

---
+++ invert-binary-tree/python.py
@@ -0,0 +1,19 @@
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def invertBinaryTree(tree):
+	if tree is None:
+		return
+    left = tree.left
+	right = tree.right
+	tree.left = right
+	tree.right = left
+	
+	invertBinaryTree(tree.right)
+	invertBinaryTree(tree.left)
+
+
---
+++ invert-binary-tree/typescript.ts
@@ -0,0 +1,31 @@
+class BinaryTree {
+  value: number;
+  left: BinaryTree | null;
+  right: BinaryTree | null;
+
+  constructor(value: number) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+/**
+ * Recursive Solution - O(n) time | O(d) space where d = depth
+ */
+export const invertBinaryTree = (tree: BinaryTree | null) => {
+  // If we reach the end of a branch
+  if (!tree) {
+    return;
+  }
+
+  // swap out left and right
+  let right = tree.right;
+  let left = tree.left;
+  tree.right = left;
+  tree.left = right;
+
+  // recursively call the function on the right/left branches.
+  // If they are null this is handled by the check on line 15
+  invertBinaryTree(tree.right);
+  invertBinaryTree(tree.left);
+};
