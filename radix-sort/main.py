0b668397debd7af2df6f6d95e793c27d82dc8ce2
Sean Barry <se@nbarry.co.uk>
2021-05-13 23:02:39 +0100

    fix: format python

--- radix-sort/main.py
+++ radix-sort/main.py
@@ -1,33 +1,34 @@
 def radixSort(array):
-    if (len(array) == 0):
+    if len(array) == 0:
         return array
-	
+
     maxNumber = str(max(array))
     maxNumberLength = len(maxNumber)
-	
+
     for i in range(maxNumberLength):
         countingSort(array, i)
-		
+
     return array
 
+
 def countingSort(array, digit):
-	sortedArray = [0] * len(array)
-	countArray = [0] * 10
-	
-	digitColumn = 10 ** digit
-	
-	for num in array:
-		countIndex = (num // digitColumn) % 10
-		countArray[countIndex] += 1
-		
-	for idx in range(1, 10):
-		countArray[idx] += countArray[idx - 1]
-		
-	for idx in range(len(array) -1, -1, -1):
-		countIndex = (array[idx] // digitColumn) % 10
-		countArray[countIndex] -= 1
-		sortedIndex = countArray[countIndex]
-		sortedArray[sortedIndex] = array[idx]
-		
-	for idx in range(len(array)):
-		array[idx] = sortedArray[idx]
+    sortedArray = [0] * len(array)
+    countArray = [0] * 10
+
+    digitColumn = 10 ** digit
+
+    for num in array:
+        countIndex = (num // digitColumn) % 10
+        countArray[countIndex] += 1
+
+    for idx in range(1, 10):
+        countArray[idx] += countArray[idx - 1]
+
+    for idx in range(len(array) - 1, -1, -1):
+        countIndex = (array[idx] // digitColumn) % 10
+        countArray[countIndex] -= 1
+        sortedIndex = countArray[countIndex]
+        sortedArray[sortedIndex] = array[idx]
+
+    for idx in range(len(array)):
+        array[idx] = sortedArray[idx]
