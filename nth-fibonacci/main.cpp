5dbd389c75927150b202a1ad9a6759c664c41ef1
Sean Barry <se@nbarry.co.uk>
2021-05-19 22:44:49 +0100

    feat: add cpp nth fibonacci

---
+++ nth-fibonacci/main.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+
+using namespace std;
+
+int getNthFib(int n)
+{
+    int lastTwo[2] = {0, 1};
+    int counter = 3;
+
+    while (counter <= n)
+    {
+        int nextFib = lastTwo[0] + lastTwo[1];
+        lastTwo[0] = lastTwo[1];
+        lastTwo[1] = nextFib;
+        counter++;
+    }
+
+    return n > 1 ? lastTwo[1] : lastTwo[0];
+}
