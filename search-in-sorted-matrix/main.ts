40b232751e3900d8f30a3c345f03afffb18cc012
Sean Barry <se@nbarry.co.uk>
2021-05-22 19:44:10 +0100

    feat: search in sorted matrix typescript

---
+++ search-in-sorted-matrix/main.ts
@@ -0,0 +1,20 @@
+type Range = [number, number];
+
+const searchInSortedMatrix = (matrix: number[][], target: number): Range => {
+  let row = 0;
+  let col = matrix[0].length - 1;
+
+  while (row < matrix.length && col >= 0) {
+    if (matrix[row][col] > target) {
+      col -= 1;
+    } else if (matrix[row][col] < target) {
+      row += 1;
+    } else {
+      return [row, col];
+    }
+  }
+
+  return [-1, -1];
+};
+
+export {};
