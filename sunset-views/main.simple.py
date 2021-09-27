7f58b4584f824abc2fd161ab461219128f2c5ecb
Sean Barry <se@nbarry.co.uk>
2021-09-27 21:33:31 +0100

    fix: formatting

--- sunset-views/main.simple.py
+++ sunset-views/main.simple.py
@@ -1,25 +1,25 @@
 # O(n) time | O(n) space
 def sunsetViews(buildings, direction):
 	buildingsWithSunsetViews = []
-	
+
 	startIdx = 0 if direction == 'WEST' else len(buildings) - 1
 	step = 1 if direction == 'WEST' else -1
-	
+
 	idx = startIdx
 	runningMaxHeight = 0
-	
+
 	while idx >= 0 and idx < len(buildings):
 		buildingHeight = buildings[idx]
-		
+
 		if buildingHeight > runningMaxHeight:
 			buildingsWithSunsetViews.append(idx)
-			
+
 		runningMaxHeight = max(runningMaxHeight, buildingHeight)
-		
+
 		idx += step
-		
+
 	if direction == 'EAST':
 		return buildingsWithSunsetViews[::-1]
-	
+
 	return buildingsWithSunsetViews
 
