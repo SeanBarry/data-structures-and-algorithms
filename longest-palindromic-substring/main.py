4b36f7b88732d511f5ac846504dd246bdd0d88a8
Sean Barry <se@nbarry.co.uk>
2021-09-27 21:26:17 +0100

    fix: formatting again

--- longest-palindromic-substring/main.py
+++ longest-palindromic-substring/main.py
@@ -1,18 +1,18 @@
 def longestPalindromicSubstring(string):
 	currentLongest = [0, 1]
 
-for i in range(1, len(string)):
-	# check odd and even length palindromes
-	odd = getLongestPalindromFrom(string, i - 1, i + 1)
-	even = getLongestPalindromFrom(string, i - 1, i)
+	for i in range(1, len(string)):
+		# check odd and even length palindromes
+		odd = getLongestPalindromFrom(string, i - 1, i + 1)
+		even = getLongestPalindromFrom(string, i - 1, i)
 
-	# pick the longest of the odd/even results
-	longest = max(odd, even, key=lambda x: x[1] - x[0])
-	# set the currentLongest to the largest of the longest or the recorded longest
-	currentLongest = max(longest, currentLongest, key=lambda x: x[1] - x[0])
+		# pick the longest of the odd/even results
+		longest = max(odd, even, key=lambda x: x[1] - x[0])
+		# set the currentLongest to the largest of the longest or the recorded longest
+		currentLongest = max(longest, currentLongest, key=lambda x: x[1] - x[0])
 
-	# slice the string to return the longest palindrome. Have to +1 to the second val as range it's exclusive
-	return string[currentLongest[0] : (currentLongest[1] + 1)]
+		# slice the string to return the longest palindrome. Have to +1 to the second val as range it's exclusive
+		return string[currentLongest[0] : (currentLongest[1] + 1)]
 
 # Helper function to return the longest palindrome in a string
 def getLongestPalindromFrom(string, leftIdx, rightIdx):
