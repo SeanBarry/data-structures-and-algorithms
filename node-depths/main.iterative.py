748a162da6e03a6bd22fbdff93f8710ba0b436f4
Sean Barry <se@nbarry.co.uk>
2021-09-29 21:17:08 +0100

    fix: incorrect names and add notes

--- node-depths/main.iterative.py
+++ node-depths/main.iterative.py
@@ -4,8 +4,30 @@ class BinaryTree:
         self.left = None
         self.right = None
 
-def nodeDepths(root, depth = 0):
-    if root is None:
-        return 0
 
-    return depth + nodeDepths(root.left, depth + 1) + nodeDepths(root.right, depth + 1)
+def nodeDepths(root):
+    # initialise the sum of the depths
+    sumOfDepths = 0;
+    # create a stack with each node and its corresponding depth, ready to be processed
+    stack = [{ "node": root, "depth": 0}]
+	
+    # while there are items in the stack
+    while len(stack) > 0:
+        # grab a node from the stack
+        nodeInfo = stack.pop()
+        node, depth = nodeInfo["node"], nodeInfo["depth"]
+		
+        if node is None:
+            continue
+
+        # increase the depths sum by the depth of the node
+        sumOfDepths += depth
+
+        # append the node's children to the stack, incrementing their depth count by 1
+        stack.append({ "node": node.left, "depth": depth + 1})
+        stack.append({ "node": node.right, "depth": depth + 1})
+	
+    # once the stack is empty, return the sum of the depths
+    return sumOfDepths
+
+
