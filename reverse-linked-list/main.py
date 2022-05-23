908891e5868d0c33f8fb32fd3be90ab2d61aa221
Sean Barry <se@nbarry.co.uk>
2022-05-23 22:43:10 +0100

    chore: add documentation

--- reverse-linked-list/main.py
+++ reverse-linked-list/main.py
@@ -10,10 +10,14 @@ def reverseLinkedList(head):
 	prev = None
 
 	while current is not None:
+		# store the next node as a temp
 		next = current.next
+		# update the current's next to point to the previous
 		current.next = prev
+		# slide the pointers along by one node
 		prev = current
 		current = next
-
+	# the while loop ends when current is None
+	# so the prev will be the new head of the LL
 	return prev
 		
