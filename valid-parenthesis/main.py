f5158e3a021a9b47eba109239c0dd5073e73fe9d
Sean Barry <se@nbarry.co.uk>
2022-09-26 17:33:39 +0100

    chore: tidy up solution and add documentation

--- valid-parenthesis/main.py
+++ valid-parenthesis/main.py
@@ -1,21 +1,34 @@
+# define the corresponding parenthesis sets
+parenthesis_mapping = {
+    "{": "}",
+    "[": "]",
+    "(": ")"
+}
+
 def isValid(s: str) -> bool:
+    # initialise a stack
     stack = []
     
     for char in s:
+        # if the character is an opening parenthesis, append it to the stack
+        # and move on
         if char in ["(", "[", "{"]:
             stack.append(char)
             continue
         
+        # otherwise, if the stack is empty then it's an invalid string
         if not len(stack):
             return False
 
+        # if the stack isn't empty, grab the last item
         popped = stack.pop()
-            
-        if char == ")" and popped != "(":
+
+        # if the character isn't the corresponding parenthesis to the latest item
+        # in the stack, the string is invalid
+        if char != parenthesis_mapping[popped]:
             return False
-        elif char == "]" and popped != "[":
-            return False
-        elif char == "}" and popped != "{":
-            return False
-        
+    
+    # when we finish iterating the string, the stack may still have unclosed opening parenthesis
+    # in it. E.G. if the input string is: "(((())"
+    # As such, we want to return True only if the stack is empty, otherwise the string is invalid.
     return len(stack) == 0
